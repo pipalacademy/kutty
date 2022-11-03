6807f054c3a94866035f0a6c3d0beb4e86ea785a
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-11-03 14:34:31 +0530

    Remove Jinja from dependencies
    Remove jinja from requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ setup(
     version="0.1.0",
     description="Reusable HTML widgets for building web apps in pure Python",
     author="Pipal Academy",
-    install_requires=[
-        "Jinja2>=3.1.0",
-    ],
+    install_requires=[],
     url="https://github.com/pipalacademy/patterns",
     packages=["patterns"]
 )
