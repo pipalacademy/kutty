727aeba89a3f7a6d3f3d1ebfd5af46f2ebc28351
Anand Chitipothu <anandology@gmail.com>
2022-11-19 20:36:24 +0530

    Renamed patterns as kutty
    Issue #4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="patterns",
+    name="kutty",
     version="0.1.0",
     description="Reusable HTML widgets for building web apps in pure Python",
     author="Pipal Academy",
     install_requires=[],
-    url="https://github.com/pipalacademy/patterns",
-    packages=["patterns"]
+    url="https://github.com/pipalacademy/kutty",
+    packages=["kutty"]
 )
