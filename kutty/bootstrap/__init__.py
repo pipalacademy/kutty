ec7594b46ae6264774c77e29937e5f23d7868334
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2023-02-16 02:10:00 +0530

    Add bootstrap Hero component

--- kutty/bootstrap/__init__.py
+++ kutty/bootstrap/__init__.py
@@ -2,3 +2,4 @@ from .alert import Alert
 from .card import Card
 from .table import Table
 from .layout import Layout, Page
+from .hero import Hero
---
+++ kutty/bootstrap/hero.py
@@ -0,0 +1,119 @@
+"""The Bootstrap Hero component
+
+Usage:
+
+```python
+from kutty.bootstrap import Hero
+
+hero = Hero(
+    title="Hello, world!",
+    subtitle="This is a simple hero unit",
+    text="Hello world is a nice placeholder",
+)
+hero.add_cta("Learn More", href="/more")
+print(hero.render())
+```
+"""
+
+from kutty import Optional
+
+from .base import BootstrapElement
+
+
+class Hero(BootstrapElement):
+    TAG = "div"
+    CLASS = "jumbotron"
+
+    def __init__(
+            self,
+            *args,
+            title=None, subtitle=None, text=None,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.header = Optional(HeroHeader())
+        self.body = Optional(HeroBody())
+        separator = Optional(
+            HeroSeparator(),
+            render_condition=lambda _: (not self.header.is_empty() and
+                                        not self.body.is_empty()))
+
+        self << self.header
+        self << separator
+        self << self.body
+
+        if title:
+            self.add_title(title)
+        if subtitle:
+            self.add_subtitle(subtitle)
+        if text:
+            self.add_text(text)
+
+    def add_title(self, *args, **kwargs):
+        return self.header.add_title(*args, **kwargs)
+
+    def add_subtitle(self, *args, **kwargs):
+        return self.header.add_subtitle(*args, **kwargs)
+
+    def add_text(self, *args, **kwargs):
+        return self.body.add_text(*args, **kwargs)
+
+    def add_cta(self, *args, **kwargs):
+        return self.body.add_cta(*args, **kwargs)
+
+
+class HeroHeader(BootstrapElement):
+    TAG = "div"
+
+    def add_title(self, title):
+        title = HeroTitle(title)
+        self << title
+        return title
+
+    def add_subtitle(self, subtitle):
+        subtitle = HeroSubtitle(subtitle)
+        self << subtitle
+        return subtitle
+
+
+class HeroBody(BootstrapElement):
+    TAG = "div"
+
+    def add_text(self, text):
+        element = HeroText(text)
+        self << element
+        return element
+
+    def add_cta(self, text, href):
+        cta = HeroCTA(text=text, href=href)
+        self << cta
+        return cta
+
+
+class HeroSeparator(BootstrapElement):
+    TAG = "hr"
+    KIND = "void"
+    CLASS = "my-4"
+
+
+class HeroTitle(BootstrapElement):
+    TAG = "h1"
+    CLASS = "display-4"
+
+
+class HeroSubtitle(BootstrapElement):
+    TAG = "p"
+    CLASS = "lead"
+
+
+class HeroText(BootstrapElement):
+    TAG = "p"
+
+
+class HeroCTA(BootstrapElement):
+    TAG = "a"
+    CLASS = "btn btn-primary btn-lg"
+
+    def __init__(self, *args, text, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add(text)
