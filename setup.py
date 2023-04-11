ab1feb4312d84153a4a49497cd1723e1df2a4970
Anand Chitipothu <anandology@gmail.com>
2023-04-11 12:37:38 +0530

    Updated the Markdown version in setup.py
    Also using >= for version instead of == for better downstream compatability.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1.0",
     description="Reusable HTML widgets for building web apps in pure Python",
     author="Pipal Academy",
-    install_requires=["pygments", "markdown==3.4.1"],
+    install_requires=["pygments", "markdown=>3.4.3"],
     url="https://github.com/pipalacademy/kutty",
     packages=find_packages()
 )
